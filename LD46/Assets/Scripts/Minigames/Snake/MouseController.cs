83c8c73623909d5564eb5d3776937d6ffdb8db40
Team-on <timonsol13@gmail.com>
2020-04-19 02:31:35 +0300

    Snake minigame

--- LD46/Assets/Scripts/Minigames/Snake/MouseController.cs
+++ LD46/Assets/Scripts/Minigames/Snake/MouseController.cs
@@ -4,54 +4,54 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
-    public Vector3 gameObjectSreenPoint;
-    public Vector3 mousePreviousLocation;
-    public Vector3 mouseCurLocation;
-    private bool dragging = false;
-    private float distance;
+    [Header("Balance")]
+    [SerializeField] [Tooltip("Got squared while play")] float speed = 2;
+    [SerializeField] [Tooltip("Got squared while play")] float topSpeed = 1000;
 
+    [Header("Refs")]
+    [SerializeField] SnakeMinigame minigame = null;
+    [SerializeField] Rigidbody2D rb = null;
 
-    void OnMouseEnter()
-    {
+    Vector3 mouseCurrLocation, diff;
+    Vector3 currVelocity;
+    Vector3 force;
+    bool isDrag = false;
+
+#if UNITY_EDITOR
+    private void OnValidate() {
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+    }
+#endif
+
+    private void Awake() {
+        topSpeed *= topSpeed;
     }
 
-    void OnMouseExit()
-    {
+    void OnMouseDown() {
+        isDrag = true;
     }
 
-    void OnMouseDown()
-    {
-        gameObjectSreenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-        mousePreviousLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
-        dragging = true;
-    }
-
-    public Vector3 force;
-    public Vector3 objectCurrentPosition;
-    public Vector3 objectTargetPosition;
-    public float topSpeed = 10;
-
-    void OnMouseDrag()
-    {
-        mouseCurLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
-        force = mouseCurLocation - mousePreviousLocation;//Changes the force to be applied
-        mousePreviousLocation = mouseCurLocation;
-    }
-
-    void OnMouseUp()
-    {
-        dragging = false;
-        if (GetComponent<Rigidbody2D>().velocity.magnitude > topSpeed)
-            force = GetComponent<Rigidbody2D>().velocity.normalized * topSpeed;
+    void OnMouseUp() {
+        isDrag = false;
     }
 
     public void FixedUpdate()
     {
-        GetComponent<Rigidbody2D>().velocity = force;
+        if (isDrag && minigame.isPlaying) {
+            mouseCurrLocation = GameManager.Instance.Camera.ScreenToWorldPoint(Input.mousePosition);
+            mouseCurrLocation.z = 0.0f;
+            diff = mouseCurrLocation - transform.position;
+            force = Vector3.SmoothDamp(force, diff.normalized * diff.magnitude * speed, ref currVelocity, 0.05f);
+            if (force.sqrMagnitude > topSpeed)
+                force = force.normalized * topSpeed;
+
+            rb.velocity = force;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        minigame.Win();
     }
 }
--- LD46/Assets/Scripts/Minigames/SnakeMinigame.cs
+++ LD46/Assets/Scripts/Minigames/SnakeMinigame.cs
@@ -4,7 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class SnakeMinigame : BaseMinigame
-{
+public class SnakeMinigame : BaseMinigame {
+	public void Win() {
+		isPlaying = false;
+		ShowWinAnimation();
+	}
 
+	protected override void ShowLoseAnimation() {
+		LeanTween.delayedCall(1.0f, () => {
+			base.ShowLoseAnimation();
+		});
+	}
+
+	protected override void ShowWinAnimation() {
+		LeanTween.delayedCall(1.0f, () => {
+			base.ShowWinAnimation();
+		});
+	}
 }
