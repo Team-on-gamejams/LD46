fe7e6050187f4e448a6232575732ee2f72e47480
Long-as-Python <45416328+Long-as-Python@users.noreply.github.com>
2020-04-19 22:48:33 +0300

    crab movement

--- LD46/Assets/Scripts/Minigames/CrabMinigame.cs
+++ LD46/Assets/Scripts/Minigames/CrabMinigame.cs
@@ -4,23 +4,54 @@ using UnityEngine;
 
 public class CrabMinigame : BaseBaseMinigame
 {
-	public void Win()
-	{
-		isPlaying = false;
-		ShowWinAnimation();
-	}
 
-	protected override void ShowLoseAnimation()
-	{
-		LeanTween.delayedCall(1.0f, () => {
-			base.ShowLoseAnimation();
-		});
-	}
+    public GameObject Crab;
 
-	protected override void ShowWinAnimation()
-	{
-		LeanTween.delayedCall(1.0f, () => {
-			base.ShowWinAnimation();
-		});
-	}
+    private Vector2 targetPos;
+    public float Range = 30;
+    public float maxHeight;
+    public float minHeight;
+    public float speed;
+
+    public void GoUp()
+    {
+        if (Crab.transform.position.y < maxHeight)
+        {
+            targetPos = new Vector2(Crab.transform.position.x, Crab.transform.position.y + Range);
+            //Crab.transform.position = targetPos;
+            Crab.transform.position = Vector2.MoveTowards(Crab.transform.position, targetPos, speed * Time.deltaTime);
+        }
+
+    }
+
+    public void GoDown()
+    {
+        if (Crab.transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(Crab.transform.position.x, Crab.transform.position.y - Range);
+            Crab.transform.position = targetPos;
+        }
+    }
+
+    public void Win()
+    {
+        isPlaying = false;
+        ShowWinAnimation();
+    }
+
+    protected override void ShowLoseAnimation()
+    {
+        LeanTween.delayedCall(1.0f, () =>
+        {
+            base.ShowLoseAnimation();
+        });
+    }
+
+    protected override void ShowWinAnimation()
+    {
+        LeanTween.delayedCall(1.0f, () =>
+        {
+            base.ShowWinAnimation();
+        });
+    }
 }
