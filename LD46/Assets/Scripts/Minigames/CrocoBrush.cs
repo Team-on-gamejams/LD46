0d0406df2ef6353241786785de69aa50dcab3335
Dima Shymko <trozeman@gmail.com>
2020-04-19 22:19:54 +0300

    Brush 🐱‍🏍Stopper 😈

--- LD46/Assets/Scripts/Minigames/CrocoBrush.cs
+++ LD46/Assets/Scripts/Minigames/CrocoBrush.cs
@@ -9,11 +9,33 @@ public class CrocoBrush : MonoBehaviour
   public void OnMouseDown()
   {
     isDragging = true;
+    transform.GetComponent<BoxCollider2D>().enabled = false;
   }
 
   public void OnMouseUp()
   {
     isDragging = false;
+    transform.GetComponent<BoxCollider2D>().enabled = true;
+  }
+
+  public void OnTriggerEnter2D(Collider2D colide) {   
+    if (colide.gameObject.name == "Top")
+    {
+      isDragging = false;
+      transform.Translate(new Vector3(0f,-0.5f,0));
+    }
+
+    if (colide.gameObject.name == "Bottom")
+    {
+      isDragging = false;
+      transform.Translate(new Vector3(0f, 0.5f, 0));
+    }
+
+    if (colide.gameObject.name == "Right")
+    {
+      isDragging = false;
+      transform.Translate(new Vector3(-0.5f, 0f, 0));
+    }
   }
 
 
@@ -22,8 +44,9 @@ public class CrocoBrush : MonoBehaviour
     {
       if (isDragging)
       {
-      Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-      transform.Translate(mousePosition);
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        transform.Translate(mousePosition);
       }
+      
     }
 }
