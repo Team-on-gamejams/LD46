00dbfc88745a7571aad8a437a448bdbd79fe18ee
Dima Shymko <trozeman@gmail.com>
2020-04-19 19:22:49 +0300

    Caries List

--- LD46/Assets/Scripts/Minigames/CrocoMinigame.cs
+++ LD46/Assets/Scripts/Minigames/CrocoMinigame.cs
@@ -7,6 +7,9 @@ using TMPro;
 public class CrocoMinigame : BaseMinigame {
   public Transform Crocodile;
   public Transform Toothbrush;
+  public GameObject FrontCaries;
+  [SerializeField] List<Transform> Caries;
+  bool flip = false;
 
 	public void Win() {
 		isPlaying = false;
@@ -24,4 +27,25 @@ public class CrocoMinigame : BaseMinigame {
 			base.ShowWinAnimation();
 		});
 	}
+
+
+  void WithForeachLoop()
+  {
+    foreach (Transform child in FrontCaries.transform) Caries.Add(child);
+    print("Done");
+  }
+
+  void Awake()
+  {
+    WithForeachLoop();
+  }
+
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Mouse1))
+    {
+      flip = !flip;
+      Toothbrush.GetComponent<SpriteRenderer>().flipY = flip;
+    }
+  }
 }
