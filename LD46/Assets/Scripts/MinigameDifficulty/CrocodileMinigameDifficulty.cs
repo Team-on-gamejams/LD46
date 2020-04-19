bb1e94491fcdb9f8a7d9bb8501eaca50142c4a3b
Team-on <timonsol13@gmail.com>
2020-04-19 21:08:27 +0300

    Crocodile difficulty

---
+++ LD46/Assets/Scripts/MinigameDifficulty/CrocodileMinigameDifficulty.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Crocodile", menuName = "MinigameDifficulty/Crocodile")]
+public class CrocodileMinigameDifficulty : BaseMinigameDifficulty {
+	
+}
