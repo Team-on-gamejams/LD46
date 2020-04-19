026accb43f916a9330fb05978973c67018a72867
Long-as-Python <45416328+Long-as-Python@users.noreply.github.com>
2020-04-19 20:45:44 +0300

    crab init

---
+++ LD46/Assets/Scripts/Minigames/CrabMinigame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabMinigame : BaseBaseMinigame
+{
+	public void Win()
+	{
+		isPlaying = false;
+		ShowWinAnimation();
+	}
+
+	protected override void ShowLoseAnimation()
+	{
+		LeanTween.delayedCall(1.0f, () => {
+			base.ShowLoseAnimation();
+		});
+	}
+
+	protected override void ShowWinAnimation()
+	{
+		LeanTween.delayedCall(1.0f, () => {
+			base.ShowWinAnimation();
+		});
+	}
+}
