75fce0252af1de798b2fe9a227fc7c7e883dfe1b
Long-as-Python <45416328+Long-as-Python@users.noreply.github.com>
2020-04-20 20:44:16 +0300

    Crab lose
    now you caan lose in crab game

--- LD46/Assets/Scripts/Minigames/Crab/Obstacle.cs
+++ LD46/Assets/Scripts/Minigames/Crab/Obstacle.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    private GameObject Crab;
+
+    private void Start()
+    {
+        Crab = GameObject.FindWithTag("Crab");
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name=="crab" || other.gameObject.name == "die")
+        if (other.gameObject.name == "crab")
         {
+            Crab.GetComponent<CrabMinigame>().Lose();
             Destroy(gameObject);
         }
+        else if (other.gameObject.name == "die")
+            Destroy(gameObject);
     }
 }
--- LD46/Assets/Scripts/Minigames/CrabMinigame.cs
+++ LD46/Assets/Scripts/Minigames/CrabMinigame.cs
@@ -86,6 +86,12 @@ public class CrabMinigame : BaseMinigame
             spawnTimeObstacle -= Time.deltaTime;
         }
     }
+
+    public void Lose()
+    {
+        ShowLoseAnimation();
+        isPlaying = false;
+    }
     //Obstacle end
     public void Win()
     {
