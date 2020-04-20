a147e5a11e4b1d86c28634b3ad097eef67438578
Dima Shymko <trozeman@gmail.com>
2020-04-21 00:43:16 +0300

    Btn sounds 🔊

---
+++ LD46/Assets/Scripts/PlayOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnClick : MonoBehaviour
+{
+  public AudioClip noise;
+  
+  public void PlayNoise()
+  {
+    AudioManager.Instance.Play(noise, channel: AudioManager.AudioChannel.Sound);
+  }
+
+}
