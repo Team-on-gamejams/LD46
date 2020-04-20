94dad9258d345b8972ca0a6b3d154a88dc4ea0ed
Team <timonsol13@gmail.com>
2020-04-20 05:11:04 +0300

    Remove debug hotkeys

--- LD46/Assets/Thirdparty/GameTemplate/Scripts/Debug/DebugHotkeys.cs
+++ LD46/Assets/Thirdparty/GameTemplate/Scripts/Debug/DebugHotkeys.cs
@@ -5,12 +5,4 @@ using Cinemachine;
 public class DebugHotkeys : MonoBehaviour {
 	CinemachineVirtualCamera cam;
 
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.R)) {
-			SceneManager.LoadScene(0);
-		}
-		else if (Input.GetKeyDown(KeyCode.Escape)) {
-			QuitGame.QuitApp();
-		}
-	}
 }
